93eb2f2bb1b251d66ac9275df01821174aed2df8
Lawunn <lawunnkhaing.tgi@gmail.com>
2024-02-18 19:01:26 +0200

    Added Blog

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -9,20 +9,19 @@ const Blogs = () => {
       <h2>Blog Articles</h2>
       <p>Find the latest articles and blog posts here.</p>
       <Accordion defaultActiveKey="0" flush> 
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          
         <Card> 
-          <Card.Header>
-            {/* <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Accordion item #1
-            </Accordion.Toggle> */}
-
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
+          <Card.Header>Card Header #1</Card.Header>
           <Card.Body>
-            <Card.Title>Card Header #1</Card.Title>
+            <Card.Title>Card Title</Card.Title>
             <Card.Text>Here's some content for the first blog article.</Card.Text>
           </Card.Body>
-          </Accordion.Collapse>
         </Card> 
+        </Accordion.Body>
+      </Accordion.Item>
       </Accordion> 
     </div>
   );
